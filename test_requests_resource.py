569d6e17b7786597bc5e5529a837728280dc2344
denniswanjiru <denniswanjiru71@gmail.com>
2018-05-30 01:32:41 +0300

    [Feature #157930785] Add test edges cases and refactor code

--- test_requests_resource.py
+++ test_requests_resource.py
@@ -1,5 +1,7 @@
+
 from app import app
 import unittest
+import json
 
 
 class TestRequestResource(unittest.TestCase):
@@ -17,17 +19,24 @@ class TestRequestResource(unittest.TestCase):
         }
 
     def test_get_requests(self):
+        response = self.client.post(
+            'api/v1/users/requests/', data=self.data["request"])
+        self.assertEqual(response.status_code, 201)
         response = self.client.get('api/v1/users/requests/')
         self.assertEqual(response.status_code, 200)
 
     def test_get_a_request(self):
-        response = self.client.get('api/v1/users/requests/1/')
+        response = self.client.post(
+            'api/v1/users/requests/', data=self.data["request"])
+        self.assertEqual(response.status_code, 201)
+        created_resource = json.loads(response.data)
+        response = self.client.get(
+            f'api/v1/users/requests/{created_resource["request_id"]}/')
         self.assertEqual(response.status_code, 200)
 
     def test_post_a_request(self):
         response = self.client.post(
             'api/v1/users/requests/', data=self.data["request"])
-
         self.assertEqual(response.status_code, 201)
 
     def test_400_post_a_request(self):
@@ -48,17 +57,33 @@ class TestRequestResource(unittest.TestCase):
         self.assertEqual(bad_data.status_code, 400)
 
     def test_update_a_request(self):
+        response = self.client.post(
+            'api/v1/users/requests/', data=self.data["request"])
+        self.assertEqual(response.status_code, 201)
+        created_resource = json.loads(response.data)
         response = self.client.put(
-            'api/v1/users/requests/1/', data=self.data["request"])
-
+            f'api/v1/users/requests/{created_resource["request_id"]}/', data={
+                "title": "My Actual request",
+                "request_type": "repair"
+            })
         self.assertEqual(response.status_code, 200)
 
     def test_400_update_a_request(self):
-        empty_dict = self.client.put('api/v1/users/requests/1', data={})
-        empty_tuple = self.client.post('api/v1/users/requests/', data=())
-        empty_list = self.client.post('api/v1/users/requests/', data=[])
-        empty_string = self.client.post('api/v1/users/requests/', data="")
-        bad_data = self.client.post('api/v1/users/requests/', data={
+        response = self.client.post(
+            'api/v1/users/requests/', data=self.data["request"])
+        self.assertEqual(response.status_code, 201)
+
+        created_resource = json.loads(response.data)
+
+        empty_dict = self.client.put(
+            f'api/v1/users/requests/{created_resource["request_id"]}/', data={})
+        empty_tuple = self.client.put(
+            f'api/v1/users/requests/{created_resource["request_id"]}/', data=())
+        empty_list = self.client.put(
+            f'api/v1/users/requests/{created_resource["request_id"]}/', data=[])
+        empty_string = self.client.put(
+            f'api/v1/users/requests/{created_resource["request_id"]}/', data="")
+        bad_data = self.client.put(f'api/v1/users/requests/{created_resource["request_id"]}/', data={
             "request_id": 1,
             'title': '',
             "location": 234,
@@ -74,12 +99,16 @@ class TestRequestResource(unittest.TestCase):
         response = self.client.post(
             'api/v1/users/requests/', data=self.data["request"])
 
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 201)
 
-        res = self.client.delete('api/v1/users/requests/1/')
+        created_resource = json.loads(response.data)
+
+        res = self.client.delete(
+            f'api/v1/users/requests/{created_resource["request_id"]}/')
         self.assertEqual(res.status_code, 200)
 
-        request_res = self.client.get('api/v1/users/requests/1')
+        request_res = self.client.get(
+            f'api/v1/users/requests/{created_resource["request_id"]}/',)
         self.assertEqual(request_res.status_code, 404)
 
 
