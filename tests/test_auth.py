49061025c50aa301bfbedf6c64f6733bce32c927
denniswanjiru <denniswanjiru71@gmail.com>
2018-06-06 12:29:50 +0300

    [Chore #158141259] Add test to handle username uniqueness

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -42,7 +42,7 @@ class TestRequestResource(unittest.TestCase):
             data=json.dumps(self.data["user"]),
             content_type=("application/json")
         )
-        print(response.data)
+
         self.assertEqual(response.status_code, 201)
 
     def test_signin(self):
@@ -68,6 +68,22 @@ class TestRequestResource(unittest.TestCase):
         # Sign the user out
         response = self.client.post('/api/v1/users/auth/signout/')
         self.assertEqual(response.status_code, 200)
+
+    def test_username_taken(self):
+        """ Test if the username is already taken """
+        self.client.post(
+            '/api/v1/users/auth/signup/',
+            data=json.dumps(self.data["user"]),
+            content_type=("application/json")
+        )
+
+        response = self.client.post(
+            '/api/v1/users/auth/signup/',
+            data=json.dumps(self.data["user"]),
+            content_type=("application/json")
+        )
+
+        self.assertEqual(response.status_code, 400)
 
 
 if __name__ == '__main__':
