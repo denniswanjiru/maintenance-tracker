1d007e65d7fce72fca699df752600ad55661d238
denniswanjiru <denniswanjiru71@gmail.com>
2018-06-07 07:20:09 +0300

    [Chore #158160894] Create a query for creating a new users table

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ import psycopg2
 import os
 
 
-def Store():
+class Store():
     def __init__(self):
         self.db_host = os.getenv('DB_HOST')
         self.db_name = os.getenv('DB_NAME')
@@ -10,3 +10,21 @@ def Store():
         self.db_password = os.getenv('DB_PASSWORD')
         self.conn = psycopg2.connect(
             database=self.db_name, host=self.db_host, user=self.db_username, password=self.db_password)
+        self.cur = self.conn.cursor()
+
+    def create_table(self):
+        self.cur.execute(
+            """
+            CREATE TABLE users(
+                id serial PRIMARY KEY,
+                username varchar NOT NULL UNIQUE,
+                name varchar NOT NULL,
+                email varchar NOT NULL UNIQUE,
+                password_hash text NOT NULL
+            );
+            """
+        )
+        self.save()
+
+    def save(self):
+        self.conn.commit()
