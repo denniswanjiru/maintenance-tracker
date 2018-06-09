a1168684835117e9b49bcaad018a91c700e70da4
denniswanjiru <denniswanjiru71@gmail.com>
2018-06-09 18:22:55 +0300

    [Feature #158138451] Implement request approval endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -153,6 +153,22 @@ class AdminRequests(Resource):
         return {'message': 'Access Denied'}, 403
 
 
+class ApproveRequest(Resource):
+    @jwt_required
+    def put(self, request_id):
+        current_user = get_jwt_identity()
+        if current_user['is_admin']:
+            new_req = RequestModel()
+            req = new_req.fetch_by_id(request_id)
+            if not req:
+                return {'message': f'No request found under {request_id} id'}, 404
+            elif req["status"] != 'pending':
+                return {'message': 'You can\'t approve this request, it\'s already been approved or rejected'}, 403
+            new_req.approve(request_id)
+            return {'message': 'Request Approved succcessfully'}, 200
+        return {'message': 'Access Denied'}, 403
+
+
 class UserRegistration(Resource):
     """ User Registration Resource """
     parser = reqparse.RequestParser()
@@ -226,6 +242,8 @@ class UserSignin(Resource):
 api.add_resource(RequestList, '/api/v2/users/requests/')
 api.add_resource(Request, '/api/v2/users/request/<string:request_id>/')
 api.add_resource(AdminRequests, '/api/v2/requests/')
+api.add_resource(
+    ApproveRequest, '/api/v2/requests/<string:request_id>/approve/')
 api.add_resource(UserRegistration, '/api/v2/users/auth/signup/')
 api.add_resource(UserSignin, '/api/v2/users/auth/signin/')
 # api.add_resource(UserSignout, '/api/v2/users/auth/signout/')
