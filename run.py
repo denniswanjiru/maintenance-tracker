db5586e1567f3849d0ccf2f6c53a9acdfee5ad7f
denniswanjiru <denniswanjiru71@gmail.com>
2018-06-11 00:29:32 +0300

    Add functionality to create, drop and add a superadmin via cli

--- run.py
+++ run.py
@@ -1,14 +1,23 @@
 from app import app
 from db import init, drop
+from app.models import User
 
 
 @app.cli.command()
 def init_db():
-    ''' Create tables '''
+    ''' Creates tables '''
     init()
 
 
 @app.cli.command()
 def drop_db():
-    ''' Drop tables'''
+    ''' Drops tables'''
     drop()
+
+
+@app.cli.command()
+def create_admin():
+    """ Creates a Super Admin """
+    admin = User(username="admin", name="Super Admin",
+                 password="secret", email="admin@app.com")
+    admin.add()
